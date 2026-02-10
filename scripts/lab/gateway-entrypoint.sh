769f6b022cc9bfcac77062f21d0acd1035d58d31
Léon <git.staleness589@passmail.com>
2026-02-10 15:46:16 +0100

    attempt at fixing a weird issue

--- scripts/lab/gateway-entrypoint.sh
+++ scripts/lab/gateway-entrypoint.sh
@@ -29,9 +29,30 @@ iptables -A FORWARD -i "${WAN_IFACE}" -o "${LAN2_IFACE}" -m state --state RELATE
 iptables -t nat -A POSTROUTING -o "${WAN_IFACE}" -j MASQUERADE 2>/dev/null || true
 iptables -A OUTPUT -p icmp --icmp-type redirect -j DROP 2>/dev/null || true
 
-sed -i "s/LAN_IFACE/${LAN_IFACE}/" /etc/suricata/suricata.yaml
-sed -i "s/LAN2_IFACE/${LAN2_IFACE}/" /etc/suricata/suricata.yaml
 sed -i "s|HOME_NET: \".*\"|HOME_NET: \"${HOME_NET}\"|" /etc/suricata/suricata.yaml
+
+awk -v lan="${LAN_IFACE}" -v lan2="${LAN2_IFACE}" '
+	BEGIN { inpcap = 0 }
+	/^pcap:/ {
+		print "pcap:";
+		print "  - interface: " lan;
+		print "    buffer-size: 67108864";
+		print "    checksum-checks: auto";
+		print "  - interface: " lan2;
+		print "    buffer-size: 67108864";
+		print "    checksum-checks: auto";
+		inpcap = 1;
+		next
+	}
+	inpcap {
+		if ($0 ~ /^[^[:space:]]/) {
+			inpcap = 0;
+			print
+		}
+		next
+	}
+	{ print }
+' /etc/suricata/suricata.yaml > /etc/suricata/suricata.yaml.tmp && mv /etc/suricata/suricata.yaml.tmp /etc/suricata/suricata.yaml
 
 echo "Starting Suricata (pcap on ${LAN_IFACE}, ${LAN2_IFACE})"
 exec suricata -c /etc/suricata/suricata.yaml --pcap -v
